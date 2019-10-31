58cbb27b06cbecd564302d211ba1b48c3542c9d7
jordan.besly <jordan.besly@transatel.com>
2019-10-31 15:41:10 +0100

    Basic helm integration + formatting

--- prf-theme.el
+++ prf-theme.el
@@ -32,6 +32,10 @@
 ;; TODO: prf/theme/theme-list change detection (via a clone) -> to prevent having to cycle whole list before reloading
 
 ;; ------------------------------------------------------------------------
+
+(require 'dash)
+
+;; ------------------------------------------------------------------------
 ;; INTERNAL VARS
 
 (defvar prf/theme/current-theme nil)
@@ -45,13 +49,11 @@
 
 (defun prf/deftheme/apply-theme (theme)
   (setq prf/theme/current-theme theme)
-  (load-theme theme t)
-  ;; (message theme)
-  )
+  (load-theme theme t))
+
 
 (defun prf/deftheme/revert-theme (theme)
-  (disable-theme theme)
-  )
+  (disable-theme theme))
 
 
 ;; ------------------------------------------------------------------------
@@ -59,12 +61,11 @@
 
 (defun prf/color-theme/apply-theme (theme)
   (setq prf/theme/current-theme theme)
-  (funcall theme)
-  )
+  (funcall theme))
+
 
 (defun prf/color-theme/revert-theme (theme)
-  nil
-  )
+  nil)
 
 
 ;; ------------------------------------------------------------------------
@@ -72,43 +73,39 @@
 
 (if (>= emacs-major-version 24) ;; deftheme
     (progn
-      (defalias 'prf/theme/apply-theme 'prf/deftheme/apply-theme)
-      (defalias 'prf/theme/revert-theme 'prf/deftheme/revert-theme)
-      )
-  (progn
-    (defalias 'prf/theme/apply-theme 'prf/color-theme/apply-theme)
-    (defalias 'prf/theme/revert-theme 'prf/color-theme/revert-theme)
-    (require 'color-theme)
-    (eval-after-load "color-theme"
-      '(progn
-	 (color-theme-initialize)))
-    (setq
-     color-theme-is-global   t
-     color-theme-is-cumulative t)
-    )
-  )
+      (defalias 'prf/theme/apply-theme #'prf/deftheme/apply-theme)
+      (defalias 'prf/theme/revert-theme #'prf/deftheme/revert-theme))
+  (defalias 'prf/theme/apply-theme #'prf/color-theme/apply-theme)
+  (defalias 'prf/theme/revert-theme #'prf/color-theme/revert-theme)
+  (require 'color-theme)
+  (with-eval-after-load "color-theme"
+    (color-theme-initialize))
+  (setq color-theme-is-global   t
+        color-theme-is-cumulative t))
 
 
 ;; ------------------------------------------------------------------------
 ;; FUNCTIONS: theme list
 
+(defun prf/theme-list/changed-p ()
+  (not (eq prf/theme/theme-list-change-detection prf/theme/theme-list)))
+
 (defun prf/theme-list/get-current ()
-  (car prf/theme/current-theme-list)
-  )
+  (car prf/theme/current-theme-list))
+
 
 (defun prf/theme-list/move-to-next ()
-  (setq prf/theme/current-theme-list (cdr prf/theme/current-theme-list))
-  )
+  (setq prf/theme/current-theme-list (cdr prf/theme/current-theme-list)))
+
 
 (defun prf/theme-list/end-p ()
-  (null prf/theme/current-theme-list)
-  )
+  (null prf/theme/current-theme-list))
 
 
 ;; ------------------------------------------------------------------------
 ;; FUNCTIONS: INTERACTIVE
 
-(defun prf/theme/set-default-theme ()
+(defun prf/theme/set-default-theme (&optional apply)
   (interactive)
 
   (if (null prf/theme/theme-list-change-detection)
@@ -117,31 +114,59 @@
   (if (not (null prf/theme/current-theme-list))
       (prf/theme/revert-theme prf/theme/current-theme) )
   (setq prf/theme/current-theme-list prf/theme/theme-list)
-  (prf/theme/apply-theme (car prf/theme/current-theme-list))
-  )
+  (when apply
+    (prf/theme/apply-theme (car prf/theme/current-theme-list))))
+
 
 (defun prf/theme/cycle-theme ()
   (interactive)
   (prf/theme/revert-theme prf/theme/current-theme)
 
-  (if (not (eq prf/theme/theme-list prf/theme/theme-list-change-detection))
+  (if (prf/theme-list/changed-p)
       (progn
 	(setq prf/theme/theme-list-change-detection nil)
-	(prf/theme/set-default-theme) )
-    (progn
-      (prf/theme-list/move-to-next)
-      (if (prf/theme-list/end-p)
-	  (prf/theme/set-default-theme)
-	(prf/theme/apply-theme (prf/theme-list/get-current)) )
-      )
-    )
-  )
+	(prf/theme/set-default-theme))
+    (prf/theme-list/move-to-next)
+    (when (prf/theme-list/end-p)
+      (prf/theme/set-default-theme)))
+
+  (prf/theme/apply-theme (prf/theme-list/get-current)))
+
+
+(defun prf/theme/set-theme-from-list (theme)
+  (when (stringp theme)
+    (setq theme (intern theme)))
+
+  (when (not (member theme prf/theme/theme-list))
+    (error "Theme `%s' not in `prf/theme/theme-list' %S" theme prf/theme/theme-list))
+
+  (prf/theme/revert-theme prf/theme/current-theme)
+  (setq prf/theme/current-theme-list (cons theme
+                                           (car (cdr (-split-on theme prf/theme/theme-list)))))
+  (prf/theme/apply-theme (prf/theme-list/get-current)))
+
 
 (defun prf/theme/initialize ()
   (interactive)
-  (prf/theme/set-default-theme)
-  )
+  (prf/theme/set-default-theme t))
 
+
+;; ------------------------------------------------------------------------
+;; HELM INTEGRATION
+
+(with-eval-after-load "helm"
+
+  (setq prf-theme--helm-source
+        (helm-build-sync-source "Change theme to"
+          :candidates prf/theme/theme-list
+          :fuzzy-match t
+          :action #'prf/theme/set-theme-from-list))
+
+  (defun helm-prf-theme-choose ()
+    (interactive)
+    (let ((helm-candidate-number-limit 10000))
+      (helm :sources '(prf-theme--helm-source)
+            :buffer "*helm Choose Theme*"))))
 
 ;; ------------------------------------------------------------------------
 
